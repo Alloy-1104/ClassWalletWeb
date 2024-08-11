aa257624987bedb04898500ac39b328c79baa7af
Alloy-1104 <crafter.alloy@gmail.com>
2024-08-11 23:32:40 +0900

    update

--- cww/__init__.py
+++ cww/__init__.py
@@ -1,8 +1,75 @@
 from flask import Flask
+from flask import render_template, url_for, request, redirect, flash, session
+from flask_migrate import Migrate
+
+from flask_login import LoginManager, login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+from cww.models import db, User, Wallet, Part
+
+base_dir = os.path.dirname(__file__)
 app = Flask(__name__)
 
-from cww.views import *
-import cww.models
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'cww.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = os.urandom(24)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_email):
+    return User.query.get(user_email)
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html", user_name=session["user_name"], email=session["email"])
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for("verify"))
+
+@app.route("/verify", methods=["GET", "POST"])
+def verify():
+    if request.method == "GET":
+        return render_template("verify.html")
+    elif request.method == "POST":
+        mode = request.form.get("submit")
+        if mode == "register":
+            email = request.form.get("email")
+            user_name = request.form.get("user_name")
+            password = request.form.get("password")
+            password_hash = generate_password_hash(password, method="pbkdf2:sha256")
+            user = User(email=email, user_name=user_name, password_hash=password_hash, authorized_name="")
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            return redirect("/")
+        elif mode == "login":
+            email = request.form.get("email")
+            password = request.form.get("password")
+            user = User.query.filter_by(email=email).first()
+            if not user or not user.verify_password(password):
+                flash("パスワードが違います")
+                return redirect(url_for("verify"))
+            flash("ログインしました")
+            login_user(user)
+            print(user.user_name)
+            session["user_name"] = user.user_name
+            session["email"] = user.email
+            return redirect("/")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("verify"))
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- cww/models.py
+++ cww/models.py
@@ -1,3 +1,45 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+  __tablename__ = "user"
+
+  email = db.Column(db.String(160), primary_key=True, nullable=False)
+  user_name = db.Column(db.String(40), nullable=False)
+  password_hash = db.Column(db.String(200), nullable=False)
+  authorized_name = db.Column(db.String(40), nullable=False)
+
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attribute')
+
+  @password.setter
+  def password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+  def get_id(self):
+    return self.email
+
+class Wallet(db.Model):
+  __tablename__ = "wallet"
+
+  id = db.Column(db.String(8), primary_key=True, nullable=False)
+  wallet_name = db.Column(db.String(8), nullable=False)
+
+  parts = db.relationship("Part", backref="wallet", lazy=True)  
+
+class Part(db.Model):
+  __tablename__ = "part"
+
+  id = db.Column(db.String(8), primary_key=True, nullable=False)
+  part_name = db.Column(db.String(40), nullable=False)
+  parent_wallet_id = db.Column(db.String(8), db.ForeignKey("wallet.id"), nullable=False)
+  
